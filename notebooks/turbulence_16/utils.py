5e7460818d8bc92b5ad4c6cbd774ff7f0b52059d
Zheyuan Hu <zh2095@log-3.nyu.cluster>
2021-11-03 05:15:29 -0400

    scaler

--- notebooks/turbulence_16/utils.py
+++ notebooks/turbulence_16/utils.py
@@ -3,6 +3,7 @@ import os
 import numpy as np
 import torch 
 import torch.nn as nn
+from sklearn.preprocessing import StandardScaler
 from _arfima import arfima
 from utils import *
 from athena_read import *
@@ -34,28 +35,27 @@ def to_windowed(data,meshed_blocks,window_size,pred_size):
 
     return np.array(out)
 
-def train_test_val_split(x_vals , meshed_blocks, train_proportion = 0.6, val_proportion = 0.2, test_proportion = 0.2\
+def train_test_val_split(data , meshed_blocks, train_proportion = 0.6, test_proportion = 0.2, val_proportion = 0.2\
               , window_size = 12, pred_size = 1):
+
+    scaler = StandardScaler()
+    data = scaler.fit_transform(data.reshape(-1, 1)).reshape(-1)
+
+    x_vals = to_windowed(data, meshed_blocks,window_size,pred_size)
 
     total_len = len(x_vals)
     train_len = int(total_len*train_proportion)
     val_len = int(total_len*val_proportion)
-    test_len = int(total_len*test_proportion)
+    
+    train = x_vals[0:train_len]
+    val = x_vals[train_len:(train_len+val_len)]
+    test = x_vals[(train_len+val_len):]
 
+    train_data = torch.from_numpy(train).float()
+    val_data = torch.from_numpy(val).float()
+    test_data = torch.from_numpy(test).float()
 
-    train_data = x_vals[0:train_len]
-    val_data = x_vals[train_len:(train_len+val_len)]
-    test_data = x_vals[(train_len+val_len):]
-
-    train = to_windowed(train_data,meshed_blocks,window_size,pred_size)
-    val = to_windowed(val_data,meshed_blocks,window_size,pred_size)
-    test = to_windowed(test_data,meshed_blocks,window_size,pred_size)
-
-    train = torch.from_numpy(train).float()
-    val = torch.from_numpy(val).float()
-    test = torch.from_numpy(test).float()
-
-    return train,val,test,train_data,val_data,test_data
+    return train_data,val_data,test_data
 
 class CustomDataset(torch.utils.data.Dataset):
     def __init__(self,x):
@@ -67,7 +67,7 @@ class CustomDataset(torch.utils.data.Dataset):
     def __getitem__(self,idx):
         return(self.x[idx][0].view(-1,1), self.x[idx][1].view(-1,1))
     
-def get_data_loaders(train_proportion = 0.5, test_proportion = 0.25, val_proportion = 0.25,window_size = 10, \
+def get_data_loaders(train_proportion = 0.5, test_proportion = 0.25, val_proportion = 0.25, window_size = 10, \
     pred_size =1, batch_size = 16, num_workers = 1, pin_memory = True, test_mode = False): 
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -77,9 +77,9 @@ def get_data_loaders(train_proportion = 0.5, test_proportion = 0.25, val_proport
     data_path='/scratch/zh2095/data_turb_dedt1_16'
     data, meshed_blocks = get_rho(data_path)
     
-    train_data,val_data,test_data,train_original,val_original,test_original = train_test_val_split(\
+    train_data,val_data,test_data,scaler = train_test_val_split(\
         data , meshed_blocks,train_proportion = train_proportion\
-        , val_proportion = val_proportion, test_proportion = test_proportion\
+        , test_proportion = test_proportion, val_proportion = val_proportion\
         , window_size = window_size, pred_size = pred_size)
     if test_mode:
         train_val_data = torch.cat((train_data,val_data),0)
